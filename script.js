2dc0bf403f1aa51d153d495db6c857bf3e485766
LiviaCCO <livia.ccapistrano@gmail.com>
2023-01-17 13:43:01 -0300

    versao com bonus

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
+//Iniciando com a qtd de cartas desejada
 let numCartas = Number(prompt("Com quantas cartas deseja jogar? (Lembre-se de digitar um número par entre 4 e 14)"));
 let numPar = (numCartas) % 2;
-// verificando a condição do número de cartas
-while ((numCartas < 4) || (numCartas > 14) || (numCartas == NaN) || ((numCartas)%2 != 0)){
-    alert("Você digitou um número inválido! Tente novamente!");
-    numCartas = Number(prompt("Com quantas cartas deseja jogar? (Lembre-se de digitar um número par entre 4 e 14)"));  
-}
-// while (numCartas > 14){
-//     alert("Você digitou um número inválido! Tente novamente!");
-//     numCartas = Number(prompt("Com quantas cartas deseja jogar? (Lembre-se de digitar um número par entre 4 e 14)"));  
-// }
-
-// while (numPar != 0){
-//     alert("Você digitou um número inválido! Tente novamente!");
-//     numCartas = Number(prompt("Com quantas cartas deseja jogar? (Lembre-se de digitar um número par entre 4 e 14)"));  
-// }
 
 let baralho = ['bobros' , 'explody' , 'fiesta' , 'metal' , 'revertit' , 'triplets' , 'unicorn'];
 let cartasEscolhidas = [];
 let parCartas = [];
 
-lancarCartas();
+iniciar();
+
+function iniciar(){
+    // verificando a condição do número de cartas
+    while ((numCartas < 4) || (numCartas > 14) || (numCartas == NaN) || ((numCartas)%2 != 0)){
+        alert("Você digitou um número inválido! Tente novamente!");
+        numCartas = Number(prompt("Com quantas cartas deseja jogar? (Lembre-se de digitar um número par entre 4 e 14)"));  
+    }
+    lancarCartas();
+}
+
+//para montar as cartas na qtd desejada pelo usuário
 function lancarCartas(){
     let i=0;
     // definindo o número de pares
@@ -37,7 +35,6 @@ function lancarCartas(){
     // embaralhando as cartas:
     parCartas.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
 
-    // Esta função pode ficar separada do código acima, onde você preferir
     function comparador() { 
         return Math.random() - 0.5; 
     }
@@ -66,13 +63,8 @@ let jogadas=0;
 let total=0;
 
 
-console.log('contador inicial');
-console.log(contador);
-
 function virar(cartaClicada){
 
-    console.log(contador);
-    console.log(cartaClicada);
     // para virar até 2 cartas por vez:
     if (contador<2){
     
@@ -84,80 +76,104 @@ function virar(cartaClicada){
         cartaClicada.querySelector(".front").classList.add('vira-front');
         contador++
         jogadas++
-        console.log('contador dentro do if');
-        console.log(contador);
-       
+               
         // verificar se é a primeira carta selecionada
         const primeiraCarta = document.querySelector(".primeira");
 
-        // se for a primeira carta, marcar como primeira
+        // se não for a primeira carta, marcar como primeira
         if (primeiraCarta == null){
-            
             cartaClicada.classList.add('primeira');
-            console.log("primeira carta");
-
-            //se não for a primeira, verificar a igualdade das imagens:
         }
+        //se não for a primeira, verificar a igualdade das imagens:
         else{ 
             // A propriedade src retorna o valor do atributo src de uma imagem.
             const segundaImg = cartaClicada.querySelector("img");
             const primeiraImg = primeiraCarta.querySelector("img");
-            // const contador = 0;
-            // console.log('zerando o contador');
-            // console.log(contador);
             
-            console.log(primeiraImg.src == segundaImg.src); 
-
             // se forem diferentes, desvirar cartas, tirar a classificação primeira carta e zerar o contador
             if (primeiraImg.src != segundaImg.src){
 
-                // primeiraCarta.classList.remove('vira-back vira-front primeira');
-                // cartaClicada.classList.remove('vira-back vira-front');
-                console.log("cartas diferentes");
-                console.log('primeiraCarta')
-                console.log(primeiraCarta);
-
-                console.log('zerando o contador das cartas diferentes');
-                console.log(contador);
-
+                // para dar tempo da carta ser virada, antes de desvirar, colocamos um atraso de 1s           
                 setTimeout(desvirar, 1000);
 
                 function desvirar() {
+                    //desvira a primeira carta e tira a classificação de primeira
                     document.querySelector(".primeira .back").classList.remove('vira-back');
                     document.querySelector(".primeira .front").classList.remove('vira-front');
                     document.querySelector(".primeira").classList.remove('primeira');
-
+                    
+                    //desvira a segunda carta
                     cartaClicada.querySelector(".back").classList.remove('vira-back');
                     cartaClicada.querySelector(".front").classList.remove('vira-front');
 
+                    //zerando o contador para nova jogada
                     contador = 0;
-
-                    //Zerando o contador:
-                    // let contador=0;
                 }
              
             }
-            // se forem iguais, manter virada e tirar a classificação primeira carta
+            // se forem iguais, manter virada, tirar a classificação primeira carta e zerar contador para nova jogada
             else{
                 primeiraCarta.classList.remove('primeira');
                 contador=0;
                 total+=2;
-                console.log('cartas iguais');
-                console.log('zerando o contador das cartas iguai');
-                console.log(contador);
+                
+                // para verificar se todas as cartas foram viradas
+                // se sim, encerrar a partida
                 if(total==numCartas){
                     setTimeout(fimJogo, 1000);
-                    
+                    stop();
                 }               
-
             }      
-
-        }
-        
+        } 
     } 
-    
 }
-// função para emitir alerta de fim de jogo
+// relógio
+const clock = setInterval(time, 1000);
+let tempo=0;
+function time(){
+    tempo++;
+    document.querySelector(".time").innerHTML = tempo;
+}
+// para parar o relógio
+function stop() {
+    clearInterval(clock);
+}
+
+// função para emitir alerta de fim de jogo e perguntar sobre reinicio do jogo
 function fimJogo(){
-    alert(`Você ganhou em ${jogadas} jogadas!`)
+    
+    alert(`Você ganhou em ${jogadas} jogadas! A duração do jogo foi de ${tempo} segundos!`);
+    stop();
+    // para perguntar se o usuário gostaria de reiniciar a partida.
+    let reiniciar = prompt("Gostaria de reiniciar a partida? (Digite sim ou não)");
+    
+    // while ((reiniciar !=="sim") || (reiniciar !=="não")){
+    //     reiniciar = prompt("Gostaria de reiniciar a partida? (Digite sim ou não)");
+    // }
+
+    // Se sim, reiniciar a partida:
+    if (reiniciar=="sim"){
+        //perguntar a qtd de cartas:
+        numCartas = Number(prompt("Com quantas cartas deseja jogar? (Lembre-se de digitar um número par entre 4 e 14)"));
+        //limpar a mesa
+        const card = document.querySelector("ul");
+        card.innerHTML = " "; 
+        //limpando os lets da partida anterior
+        baralho = ['bobros' , 'explody' , 'fiesta' , 'metal' , 'revertit' , 'triplets' , 'unicorn'];
+        cartasEscolhidas = [];
+        parCartas = [];
+        contador=0;
+        primeiraCarta;
+        jogadas=0;
+        total=0;
+        tempo=0;
+        setInterval(time, 1000);
+        //reiniciando a partida
+        iniciar();
+    }
+    else{
+        stop();
+    }
 }
+
+
